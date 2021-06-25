4ca8b6946ec9f02cae34e4ba20fc49356f5a6f1e
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-25 05:54:17 +0430

    Building expenseItems component

---
+++ src/Components/ExpenseItems/ExpenseItems.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ExpenseItems() {
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+<h2>Title</h2>
+<div>Amount</div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default ExpenseItems
