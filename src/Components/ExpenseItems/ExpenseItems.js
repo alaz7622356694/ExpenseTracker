8f953156ef48e2710f67b4e9726a02c73b5e80af
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-27 03:50:31 +0430

    Adding useState hook and a button

--- src/Components/ExpenseItems/ExpenseItems.js
+++ src/Components/ExpenseItems/ExpenseItems.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './ExpenseItems.css'
 import ExpenseDate from '../ExpenseDate/ExpenseDate'
 import Card from '../Card/Card'
-function ExpenseItems(props) {
-  
+const ExpenseItems=(props)=> {
+    
+//we declare a new state variable by calling the useState Hook.
+const[title,setTitle]=useState(props.title)
+
+const clickHandler=()=>{
+    
+setTitle('updated title')
+}
+
     return (
         <Card className="expense-item">
            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-<h2>{props.title}</h2>
-<div className='expense-item__price'>${props.amount}</div>
+
+          <h2>{title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
 
             </div>
+
+            <button onClick={clickHandler} >Change Title</button>
             
         </Card>
     )
