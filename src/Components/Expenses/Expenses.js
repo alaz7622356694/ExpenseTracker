2b3cc2593db10269d6809eb5d993f1a672fd3db9
alaz7622356694 <alireza.azari2235@outlook.com>
2021-07-01 06:20:31 +0430

    not founding message for no expense

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -14,18 +14,17 @@ const [filteredYear,setFilteredYear]=useState('2020')
 setFilteredYear(selectedYear)
 
         }
-
+//filtering year 
 const filter=props.items.filter(item=>{return item.date.getFullYear().toString()===filteredYear })
   
     return (
        
         <Card className="expenses" >
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        {filter.length===0 ? <p>No expenses found</p> : filter.map(item=>
+           <ExpenseItems key={item.id} title={item.title} amount={item.amount} date={item.date}></ExpenseItems> ) }
+                
         
-           {filter.map(item=>
-           <ExpenseItems key={item.id} title={item.title} amount={item.amount} date={item.date}></ExpenseItems> )
-           }        
-
        
     
                 
